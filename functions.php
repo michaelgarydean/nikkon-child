64cf5126f4ee40246474537032d4340220f8d7f5
Michael Dean <mike-dean1@hotmail.com>
2017-01-19 21:40:01 +0000

    initial commit of child theme

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', 'nikkon_parent_theme_enqueue_styles' );
+
+function nikkon_parent_theme_enqueue_styles() {
+    wp_enqueue_style( 'nikkon-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'nikkon-child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( 'nikkon-style' )
+    );
+
+}
